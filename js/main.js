ed6042124dfe35b8b853c8b73286f336210f01d6
Stefano Franchini <stefano.franchini77@gmail.com>
2020-12-18 16:02:52 +0100

    api & axios (da sistemare)

--- js/main.js
+++ js/main.js
@@ -2,13 +2,28 @@ let app = new Vue (
     {
         el: "#app",
         data: {
-
+            movies: [],
+            my_api_key: "abe738a72b875634baa4e3568faa8280",
+            query: "interstellar",
+            poster: "https://image.tmdb.org/t/p/w220_and_h330_face/xJHokMbljvjADYdit5fK5VQsXEG.jpg",
+            pages: 1,
         },
         methods: {
 
         },
-        mounted: {
-
+        mounted: function () {
+            axios.get("https://api.themoviedb.org/3/search/movie", {
+                params: {
+                    api_key: this.my_api_key,
+                    query: this.query,
+                    page: this.pages,
+                    language: "it-IT",
+                }
+            })
+            .then( (response) => {
+                this.movies = response.data.results;
+                console.log(this.movies);
+            } )
         }
     }
 );
