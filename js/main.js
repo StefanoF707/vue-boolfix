27b5f592bb677bd1eba1fd0ef7a70eabddd8cca7
Stefano Franchini <stefano.franchini77@gmail.com>
2020-12-21 15:31:24 +0100

    ricerca filtrata

--- js/main.js
+++ js/main.js
@@ -6,16 +6,22 @@ let app = new Vue (
             poster: "https://image.tmdb.org/t/p/w780",
             pages: 1,
             noImgFound: "img/img-not-available.png",
+            showTypes : ["Tutti", "Serie TV", "Film"],
             movies: [],
             query: "",
             searchQuery: "",
             openSearchField: false,
+            moviesApi: "https://api.themoviedb.org/3/search/movie?",
+            tvSeriesApi: "https://api.themoviedb.org/3/search/tv?",
+            multiSearchApi: "https://api.themoviedb.org/3/search/multi?",
+            filterSearch: "https://api.themoviedb.org/3/search/multi?",
+            indexActive: 0,
         },
         methods: {
             getQueryByInput: function () {
                 this.query = this.searchQuery;
 
-                axios.get("https://api.themoviedb.org/3/search/movie?", {
+                axios.get(this.filterSearch, {
                     params: {
                         api_key: this.my_api_key,
                         query: this.query,
@@ -46,7 +52,16 @@ let app = new Vue (
 
                 this.searchQuery = '';
             },
-
+            moveActiveClass: function (i) {
+                this.indexActive = i;
+                if (this.indexActive == 1) {
+                    this.filterSearch = this.tvSeriesApi;
+                } else if (this.indexActive == 2) {
+                    this.filterSearch = this.moviesApi;
+                } else {
+                    this.filterSearch = this.multiSearchApi;
+                }
+            },
         },
     }
 );
